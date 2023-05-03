7424f9160efcf4ffac7202dfb4386d31a7261e14
Evan Shortiss <evanshortiss@gmail.com>
2023-05-02 17:52:04 -0700

    feat: use kustomise to manage environments

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ const assert = require('assert')
 const { join } = require('path')
 
 test('Image URLs in ConfigMap resolve to 200 OK responses with image content-type', async (t) => {
-  const { data } = yaml.load(join(__dirname, '../k8s/config-map.yaml'))
+  const { data } = yaml.load(join(__dirname, '../k8s/base/config-map.yaml'))
   const images = JSON.parse(data['images.json'])
 
 
